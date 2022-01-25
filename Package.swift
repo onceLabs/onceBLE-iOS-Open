7e1418407056d6b0177548d24464a891593d7afb
JoeBakalor <jmbakalor@gmail.com>
2022-01-25 17:33:49 -0600

    embedd NordicDFU

--- Package.swift
+++ Package.swift
@@ -15,16 +15,19 @@ let package = Package(
             name: "onceBLE-iOS-Open",
             targets: ["onceBLE"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "NordicDFU", url: "https://github.com/NordicSemiconductor/IOS-DFU-Library.git", from: "4.5.0"),
-    ],
+//    dependencies: [
+//        // Dependencies declare other packages that this package depends on.
+//        // .package(url: /* package url */, from: "1.0.0"),
+//        .package(name: "NordicDFU", url: "https://github.com/NordicSemiconductor/IOS-DFU-Library.git", from: "4.5.0"),
+//    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .binaryTarget(
             name: "onceBLE",
-            path: "onceBLE.xcframework")
+            path: "onceBLE.xcframework"),
+        .binaryTarget(
+            name: "NordicDFU",
+            path: "NordicDFU.xcframework")
     ]
 )
