84d5df4a28f01a0ca4a691ef239bb2670eee32ae
JoeBakalor <jmbakalor@gmail.com>
2022-01-25 07:18:39 -0600

    update package def

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "onceBLE",
+    name: "onceBLE-iOS-Open",
 //    platforms: [
 //        //.macOS(.v11),
 //        .iOS(.v13),
@@ -13,7 +13,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "onceBLE",
+            name: "onceBLE-iOS-Open",
             targets: ["onceBLE"])
     ],
     dependencies: [
