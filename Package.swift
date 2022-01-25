c92e2d8f3844a29b501c4f42ef0d4d57823cc35a
JoeBakalor <jmbakalor@gmail.com>
2022-01-25 07:09:14 -0600

    package def

--- Package.swift
+++ Package.swift
@@ -5,21 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "onceBLE",
-    platforms: [
-        //.macOS(.v11),
-        .iOS(.v13),
-        //.watchOS(.v4)
-    ],
+//    platforms: [
+//        //.macOS(.v11),
+//        .iOS(.v13),
+//        //.watchOS(.v4)
+//    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "onceBLE",
             targets: ["onceBLE"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+//    dependencies: [
+//        // Dependencies declare other packages that this package depends on.
+//        // .package(url: /* package url */, from: "1.0.0"),
+//    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
