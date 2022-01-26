bc07f50d8fcb2a20fba17c7101b5c564850e177a
JoeBakalor <jmbakalor@gmail.com>
2022-01-25 19:13:04 -0600

    NordicIncluded

--- Package.swift
+++ Package.swift
@@ -25,9 +25,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .binaryTarget(
             name: "onceBLE",
-            path: "onceBLE.xcframework"),
-        .binaryTarget(
-            name: "NordicDFU",
-            path: "NordicDFU.xcframework")
+            path: "onceBLE.xcframework")
     ]
 )
